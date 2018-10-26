fc3abcea39e28da257e472c8016c3bdd7a1ff390
yanxiangda <yanxiangda1@jd.com>
2018-10-26 10:34:41 +0800

    连续子数组和

--- dynamic programming/continuous-subarray-sum.py
+++ dynamic programming/continuous-subarray-sum.py
@@ -8,6 +8,46 @@ class Solution:
         :type k: int
         :rtype: bool
         """
+        size = len(nums)
+        record = [[None for j in range(size)] for i in range(size)]
+        if self.Optimization(nums, k, 0, size - 1, record) == -2:
+            return True
+        else:
+            return False
+    '''
+    start和end表示索引
+    应该避免的是多次求和的计算，而不用避免判断是否为k的倍数的运算
+    缓存数组里面存的一定都是数字，都是不是结果的数字
+    数组和k都是非负的，所以注意0的情况处理
+    还有一个要求，子数组大小至少为2
+    还有一个坑，k是整数，不一定都是大于0的
+    '''
+    def Optimization(self, nums, k, start, end, record):
+        if start > end:
+            return 0
+        haveValue = record[start][end]
+        if haveValue != None:
+            return haveValue
+        if start == end:
+            return nums[start]
+        rightSum = self.Optimization(nums, k, start + 1, end, record)
+        leftSum = self.Optimization(nums, k, start, end - 1, record)
+        if ((rightSum == -2) | (leftSum == -2)):
+            return -2
+        currentValue = nums[start] + rightSum
+        if currentValue == 0:
+            return -2
+        if k == 0:
+            if currentValue == 0:
+                return -2
+            else:
+                record[start][end] = currentValue
+                return currentValue
+        if currentValue % abs(k) == 0:
+            return -2
+        else:
+            record[start][end] = currentValue
+            return currentValue
 
 
 
